84d640e47ba03ee54774ce7b49ab8f1a5b70f077
LekcRg <lekcrg@ya.ru>
2017-10-05 00:46:36 +0300

    Программа создана

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,15 @@ import json
 
 
 def load_data(filepath):
-    pass
+    return pretty_print_json(json.load(open(filepath, "r")))
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, sort_keys=True, ensure_ascii=False, indent=4))
 
 
 if __name__ == '__main__':
     pass
+
+
+load_data(input())
