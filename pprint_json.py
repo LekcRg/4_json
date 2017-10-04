13370b6403031651cf19a1497d5f44bd7a4b1c26
Lekc <lekcrg@ya.ru>
2017-10-05 01:38:16 +0300

    rename variable

--- pprint_json.py
+++ pprint_json.py
@@ -1,8 +1,8 @@
 import json
 
 
-def load_data(filepath):
-    return pretty_print_json(json.load(open(filepath, "r")))
+def load_data(path_to_file):
+    return pretty_print_json(json.load(open(path_to_file, "r")))
 
 
 def pretty_print_json(data):
