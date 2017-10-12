d0c13542f4bcb3fd42759e2a6d4d144180de54e1
Lekc <lekcrg@ya.ru>
2017-10-13 02:42:00 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -2,14 +2,14 @@ import json
 import sys
 
 
-def do_data_json(path_to_file):
+def open_json(path_to_file):
     with open(path_to_file, "r") as file:
         return json.load(file)
 
 
 def pretty_print_json(json_data):
-    return json.dumps(json_data, sort_keys=True, ensure_ascii=False, indent=4)
+    print(json.dumps(json_data, sort_keys=True, ensure_ascii=False, indent=4))
 
 
 if __name__ == '__main__':
-    print(pretty_print_json(do_data_json(sys.argv[1])))
+    pretty_print_json(open_json(sys.argv[1]))
